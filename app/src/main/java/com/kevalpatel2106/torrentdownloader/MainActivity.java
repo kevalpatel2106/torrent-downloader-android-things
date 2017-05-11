f88cafd3283715b963e95610f821453b0b837c64
Keval Patel <kevalpatel2106@gmail.com>
2017-05-11 21:48:25 +0530

    Initializing the project

---
+++ app/src/main/java/com/kevalpatel2106/torrentdownloader/MainActivity.java
@@ -0,0 +1,12 @@
+package com.kevalpatel2106.torrentdownloader;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
