e1c3902baeee037b64ae60adacb61c0abdb0d641
Keval <kevalpatel2106@gmail.com>
2017-05-23 14:17:23 +0530

    Making queue

--- app/src/main/java/com/kevalpatel2106/torrentdownloader/TorrentManager.java
+++ app/src/main/java/com/kevalpatel2106/torrentdownloader/TorrentManager.java
@@ -1,8 +1,11 @@
 package com.kevalpatel2106.torrentdownloader;
 
+import android.app.Service;
 import android.content.Context;
-import android.os.Environment;
+import android.content.Intent;
+import android.os.IBinder;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.turn.ttorrent.client.Client;
@@ -30,17 +33,15 @@ public class TorrentManager {
             throws IOException, NoSuchAlgorithmException {
 
         final Client client = new Client(InetAddress.getLocalHost(),
-                SharedTorrent.fromFile(torrent.getTorrentFile(),
-                        torrent.getOutputDir()));
+                SharedTorrent.fromFile(torrent.getTorrentFile(), torrent.getOutputDir()));
         client.setMaxDownloadRate(0.0);
         client.setMaxUploadRate(1.0);
-        client.download();
         client.addObserver(new Observer() {
             @Override
             public void update(Observable observer, Object arg) {
                 Client client1 = (Client) observer;
                 SharedTorrent sharedTorrent = client1.getTorrent();
-                Log.d("Progress", "Initialized? " + sharedTorrent.isInitialized()
+                Log.d("Progress", "Name: " + sharedTorrent.getName()
                         + " Complete: " + sharedTorrent.getCompletion() + "%"
                         + "\nDownload: " + (sharedTorrent.getDownloaded() / 1024) + "KB"
                         + "\nUpload: " + (sharedTorrent.getUploaded() / 1024) + "KB"
@@ -49,12 +50,25 @@ public class TorrentManager {
                 if (sharedTorrent.isComplete()) {
                     //Torrent Downloaded.
                     torrent.getTorrentFile().delete();
+
+                    startNextDownload();
                 }
             }
         });
 
         torrent.setClient(client);
         mTorrentClients.put(torrent.getKey(), torrent);
+    }
+
+    private static void startNextDownload() {
+        for (String key : mTorrentClients.keySet()) {
+            Torrent torrent = mTorrentClients.get(key);
+            if (torrent.getClient().getState() == Client.ClientState.WAITING
+                    || torrent.getClient().getState() == Client.ClientState.VALIDATING) {
+                torrent.getClient().download();
+                break;
+            }
+        }
     }
 
     public static void stop(@NonNull String key) {
@@ -67,16 +81,12 @@ public class TorrentManager {
         return torrent.getClient().getTorrent().getCompletion();
     }
 
-    public static File getOutputDir(Context context) {
+    static File getOutputDir(Context context) {
         File usbDir = new File("/mnt/usb");
         if (usbDir.exists() && usbDir.canWrite()) {
             Log.d("Output Dir", "External USB");
             return usbDir;
-        } else {
-            if (!Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).exists())
-                Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).mkdir();
-            return Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
         }
-//        return context.getExternalCacheDir();
+        return context.getExternalCacheDir();
     }
 }
